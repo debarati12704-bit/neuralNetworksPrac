044ce67cce8ec1ef9679c3c08707007e24026aa0
debarati12704-bit <debarati12704@gmail.com>
2026-08-15 17:16:14 +0530

    update perceptron.py and mlp.py with tests within same file

--- main/mlp.py
+++ main/mlp.py
@@ -1,8 +1,24 @@
+import numpy as np
 from sklearn.neural_network import MLPClassifier
-def mlpclassifier():
-    model=MLPClassifier(
-        hidden_layer_sizes=(4,),
-        activation="relu",
-        max_iter=4000
-    )
-    return model
+
+model=MLPClassifier(
+    hidden_layer_sizes=(4,),
+    activation="relu",
+    max_iter=3550,
+    random_state=23
+)
+#test XOR
+X= np.array([
+    [0,0],
+    [0,1],
+    [1,0],
+    [1,1]
+])
+#XOR Gate
+y_xor = np.array([0, 1, 1, 0])
+model.fit(X,y_xor)
+prediction=model.predict(X)
+print(
+    "\n predicted:", prediction,
+    "\n Actual:", y_xor
+)
--- main/perceptron.py
+++ main/perceptron.py
@@ -17,3 +17,32 @@ class Perceptron:
                 update=self.learning_rate*(target-predict)
                 self.weights+=update*xi
                 self.bias+=update
+
+
+
+#AND Gate
+X= np.array([
+    [0,0],
+    [0,1],
+    [1,0],
+    [1,1]
+])
+y= np.array([0,0,0,1])
+
+#test
+model=Perceptron()
+model.fit(X,y)
+predictions = model.predict(X)
+
+print("Predictions AND:", predictions)
+
+print("Weights:", model.weights)
+print("Bias:", model.bias)
+
+#XOR Gate
+y_xor = np.array([0, 1, 1, 0])
+
+model = Perceptron(learning_rate=0.1, epochs=100)
+model.fit(X, y_xor)
+
+print("Predict XOR:",model.predict(X))
